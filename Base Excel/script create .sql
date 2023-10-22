2ccc5b6c6e3d05ba841fca218add2e3e0d09d0c5
jemersonsoares <jemerson.soares@hotmail.com>
2023-10-21 22:32:25 -0300

    primeira versão

---
+++ Base Excel/script create .sql
@@ -0,0 +1,70 @@
+CREATE TABLE `dept_locations` (
+  `Dnumber` int NOT NULL,
+  `Dlocation` varchar(15) NOT NULL,
+  PRIMARY KEY (`Dnumber`,`Dlocation`),
+  CONSTRAINT `fk_dept_locations` FOREIGN KEY (`Dnumber`) REFERENCES `departament` (`Dnumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `dependent` (
+  `Essn` char(9) NOT NULL,
+  `Dependent_name` varchar(15) NOT NULL,
+  `Sex` char(1) DEFAULT NULL,
+  `Bdate` date DEFAULT NULL,
+  `Relationship` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`Essn`,`Dependent_name`),
+  CONSTRAINT `fk_dependent` FOREIGN KEY (`Essn`) REFERENCES `employee` (`Ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `departament` (
+  `Dname` varchar(15) NOT NULL,
+  `Dnumber` int NOT NULL,
+  `Mgr_ssn` char(9) NOT NULL,
+  `Mgr_start_date` date DEFAULT NULL,
+  `Dept_create_date` date DEFAULT NULL,
+  PRIMARY KEY (`Dnumber`),
+  UNIQUE KEY `unique_name_dept` (`Dname`),
+  KEY `Mgr_ssn` (`Mgr_ssn`),
+  CONSTRAINT `departament_ibfk_1` FOREIGN KEY (`Mgr_ssn`) REFERENCES `employee` (`Ssn`),
+  CONSTRAINT `chk_date_dept` CHECK ((`Dept_create_date` < `Mgr_start_date`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+
+CREATE TABLE `employee` (
+  `Fname` varchar(15) NOT NULL,
+  `Minit` char(1) DEFAULT NULL,
+  `Lname` varchar(15) NOT NULL,
+  `Ssn` char(9) NOT NULL,
+  `Bdate` date DEFAULT NULL,
+  `Address` varchar(30) DEFAULT NULL,
+  `Sex` char(1) DEFAULT NULL,
+  `Salary` decimal(10,2) DEFAULT NULL,
+  `Super_ssn` char(9) DEFAULT NULL,
+  `Dno` int NOT NULL,
+  PRIMARY KEY (`Ssn`),
+  CONSTRAINT `chk_salary_employee` CHECK ((`Salary` > 2000.0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `project` (
+  `Pname` varchar(15) NOT NULL,
+  `Pnumber` int NOT NULL,
+  `Plocation` varchar(15) DEFAULT NULL,
+  `Dnum` int NOT NULL,
+  PRIMARY KEY (`Pnumber`),
+  UNIQUE KEY `unique_project` (`Pname`),
+  KEY `fk_project` (`Dnum`),
+  CONSTRAINT `fk_project` FOREIGN KEY (`Dnum`) REFERENCES `departament` (`Dnumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `works_on` (
+  `Essn` char(9) NOT NULL,
+  `Pno` int NOT NULL,
+  `Hours` decimal(3,1) NOT NULL,
+  PRIMARY KEY (`Essn`,`Pno`),
+  KEY `fk_project_works_on` (`Pno`),
+  CONSTRAINT `fk_employee_works_on` FOREIGN KEY (`Essn`) REFERENCES `employee` (`Ssn`),
+  CONSTRAINT `fk_project_works_on` FOREIGN KEY (`Pno`) REFERENCES `project` (`Pnumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
